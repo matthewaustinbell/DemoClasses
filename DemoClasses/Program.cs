360ac7d14e9fe53b32195e089f8d5a4c31e344a3
unknown <matthewaustinbell@gmail.com>
2019-09-03 14:56:34 -0500

    udemy demo classes exersise

---
+++ DemoClasses/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DemoClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+        }
+    }
+}
